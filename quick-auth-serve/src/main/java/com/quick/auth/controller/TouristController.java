705e717bc5dccd2406fdef7d198732da07e56f14
Liujinxin <13160081226@163.com>
2022-04-09 15:47:56 +0800

    优化配置

--- quick-auth-serve/src/main/java/com/quick/auth/controller/TouristController.java
+++ quick-auth-serve/src/main/java/com/quick/auth/controller/TouristController.java
@@ -1,7 +1,7 @@
 package com.quick.auth.controller;
 
-import com.quick.auth.service.UserService;
 import com.quick.auth.config.params.ShiroCoreParameters;
+import com.quick.auth.service.UserService;
 import com.quick.auth.utils.ShiroUtil;
 import com.quick.common.utils.ip.IpUtil;
 import com.quick.common.vo.Result;
