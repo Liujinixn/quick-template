1aff47643e55d864d0b62723c43aaf26d639562f
Liujinxin <13160081226@163.com>
2022-04-14 22:19:22 +0800

    优化入参

--- quick-auth-serve/src/main/java/com/quick/auth/dto/ChangePasswordDTO.java
+++ quick-auth-serve/src/main/java/com/quick/auth/dto/ChangePasswordDTO.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * 更改密码DTO
@@ -19,17 +20,20 @@ public class ChangePasswordDTO {
      * 旧密码
      */
     @ApiModelProperty(value = "旧密码", required = true)
+    @NotEmpty(message = "旧密码 不可以为空")
     private String oldPassword;
 
     /**
      * 新密码
      */
     @ApiModelProperty(value = "新密码", required = true)
+    @NotEmpty(message = "新密码 不可以为空")
     private String newPassword;
 
     /**
      * 确认新密码
      */
     @ApiModelProperty(value = "确认新密码", required = true)
+    @NotEmpty(message = "确认新密码 不可以为空")
     private String confirmNewPassword;
 }
