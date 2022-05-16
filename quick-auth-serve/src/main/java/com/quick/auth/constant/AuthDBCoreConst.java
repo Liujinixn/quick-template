05389d5c0ecbee626b5aac923e90ae617120800c
Liujinxin <13160081226@163.com>
2022-05-17 00:28:55 +0800

    逻辑优化

---
+++ quick-auth-serve/src/main/java/com/quick/auth/constant/AuthDBCoreConst.java
@@ -0,0 +1,41 @@
+package com.quick.auth.constant;
+
+import com.quick.common.utils.constant.CoreConst;
+
+/**
+ * 核心常量
+ *
+ * @author Liujinxin
+ */
+public class AuthDBCoreConst extends CoreConst {
+
+    /**
+     * 状态有效（正常状态）
+     */
+    public static final Integer STATUS_VALID = 1;
+
+    /**
+     * 状态无效（删除状态）
+     */
+    public static final Integer STATUS_INVALID = 0;
+
+    /**
+     * 顶层菜单ID
+     */
+    public static Integer TOP_MENU_ID = 0;
+
+    /**
+     * 顶层菜单名称
+     */
+    public static String TOP_MENU_NAME = "顶层菜单";
+
+    /**
+     * 用户在线（在线）
+     */
+    public static int ONLINE = 1;
+
+    /**
+     * 用户不在线（不在线）
+     */
+    public static int NOT_ONLINE = 0;
+}
