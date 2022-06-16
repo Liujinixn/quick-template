cf351f6c613040ec23620d47d1bb7c55b2a1a4ef
Liujinxin <13160081226@163.com>
2022-06-17 00:07:15 +0800

    优化

--- quick-web/src/main/java/com/quick/QuickWebApplication.java
+++ quick-web/src/main/java/com/quick/QuickWebApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
-@EnableTransactionManagement
-@ServletComponentScan
 @SpringBootApplication
 public class QuickWebApplication {
 
